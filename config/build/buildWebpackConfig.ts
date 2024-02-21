f9cdcb1a4af49b64ff211b1c3748185251a7d27b
danilrip23 <danilrip065r@outlook.com>
2024-02-21 23:06:47 +0300

    add router i18 babel eslint

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,27 +1,28 @@
-import { buildLoader } from "./buildLoader";
-import { buildPlugins } from "./buildPlugins";
-import { buildResolvers } from "./buildResolvers";
-import { BuildOptions } from "./types/config";
-import webpack  from "webpack"
+import webpack from 'webpack';
+import path from 'path';
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
-import { buildDevServer } from "./buildDevServer";
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode, isDev } = options;
 
-export function buildWebpackConfig(options: BuildOptions):webpack.Configuration{
-    const {paths,mode,isDev} = options;
     return {
-        mode: mode,
-        entry:paths.entry,
+        mode,
+        entry: paths.entry,
         output: {
-          filename: "[name].[contenthash].js",
-          path: paths.build,
-          clean: true,
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
         },
-        module: {
-          rules: buildLoader(options),
-        },
-        resolve: buildResolvers(),
         plugins: buildPlugins(options),
-        devtool: isDev?'inline-source-map':undefined,
-        devServer: isDev? buildDevServer(options) :undefined,
-      }
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    };
 }
